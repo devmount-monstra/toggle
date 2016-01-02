6594d18d4cbcaaac86be1e1b1f1230a496cd14d6
Andreas Müller <mail@devmount.de>
2016-01-02 17:45:39 +0100

    completed: translations

--- languages/de.lang.php
+++ languages/de.lang.php
@@ -1,6 +1,13 @@
 <?php
 
-    return array(
+return array(
+    'toggle' => array(
         'Toggle' => 'Toggle',
         'Toggle plugin for Monstra.' => 'Toggle Plugin für Monstra.',
-    );
+        'Slide duration [ms]' => 'Slide Dauer [ms]',
+        'Slide easing' => 'Slide Übergang',
+        'Save' => 'Speichern',
+        'Configuration has been saved with success!' => 'Die Einstellung wurde erfolgreich gespeichert!',
+        'Request was denied. Invalid security token. Please refresh the page and try again.' => 'Die Anfrage wurde abgelehnt. Ungültiges Sicherheitstoken. Bitte erneut probieren.'
+    )
+);
--- languages/en.lang.php
+++ languages/en.lang.php
@@ -1,6 +1,13 @@
 <?php
 
-    return array(
+return array(
+    'toggle' => array(
         'Toggle' => 'Toggle',
         'Toggle plugin for Monstra.' => 'Toggle plugin for Monstra.',
-    );
+        'Slide duration [ms]' => 'Slide duration [ms]',
+        'Slide easing' => 'Slide easing',
+        'Save' => 'Save',
+        'Configuration has been saved with success!' => 'Configuration has been saved with success!',
+        'Request was denied. Invalid security token. Please refresh the page and try again.' => 'Request was denied. Invalid security token. Please refresh the page and try again.'
+    )
+);

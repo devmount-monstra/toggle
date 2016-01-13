026f7a50ee4ac19c9e5d80e0d5147dc55ce41105
Andreas Müller <mail@devmount.de>
2016-01-13 10:56:23 +0100

    improved: backend template

--- views/backend/index.view.php
+++ views/backend/index.view.php
@@ -1,35 +1,27 @@
 <h2><?php echo __('Toggle', 'toggle'); ?></h2>
 <br />
-<?php
-    echo (
-        Form::open() .
-        Form::hidden('csrf', Security::token())
-    );
+<?php echo
+    Form::open() .
+    Form::hidden('csrf', Security::token());
 ?>
 
 <div class="row">
     <div class="col-md-6">
-        <?php
-            echo (
-                Form::label('toggle_duration', __('Slide duration [ms]', 'toggle')) .
-                Form::input('toggle_duration', Option::get('toggle_duration'), array('class' => 'form-control'))
-            );
+        <?php echo
+            Form::label('toggle_duration', __('Slide duration [ms]', 'toggle')) .
+            Form::input('toggle_duration', Option::get('toggle_duration'), array('class' => 'form-control'));
         ?>
     </div>
     <div class="col-md-6">
-        <?php
-            echo (
-                Form::label('toggle_easing', __('Slide easing', 'toggle')) .
-                Form::select('toggle_easing', array('linear' => 'linear', 'swing' => 'swing'), Option::get('toggle_easing'), array('class' => 'form-control'))
-            );
+        <?php echo
+            Form::label('toggle_easing', __('Slide easing', 'toggle')) .
+            Form::select('toggle_easing', array('linear' => 'linear', 'swing' => 'swing'), Option::get('toggle_easing'), array('class' => 'form-control'));
         ?>
     </div>
 </div>
 <br />
 
-<?php
-    echo (
-        Form::submit('toggle_submitted', __('Save', 'toggle'), array('class' => 'btn btn-phone btn-primary')) .
-        Form::close()
-    );
+<?php echo
+    Form::submit('toggle_submitted', __('Save', 'toggle'), array('class' => 'btn btn-phone btn-primary')) .
+    Form::close();
 ?>

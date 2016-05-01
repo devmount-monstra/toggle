b4dde53267857ed6bf49487fba432c429c2ce598
Apromix <bees@meta.ua>
2016-05-01 17:38:38 +0300

    Added Ukrainian and Russian translation.

---
+++ languages/uk.lang.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'toggle' => array(
+        'Toggle' => 'Спойлер',
+        'Toggle plugin for Monstra.' => 'Плагін Monstra для згортання тексту.',
+        'Slide duration [ms]' => 'Тривалість ковзання [мс]',
+        'Slide easing' => 'Спосіб ковзання',
+        'Save' => 'Зберегти',
+        'Documentation' => 'Документація',
+        'Configuration has been saved with success!' => 'Конфігурація була збережена успішно!',
+        'Request was denied. Invalid security token. Please refresh the page and try again.' => 'Запит був відхилений. Невірний маркер безпеки. Будь ласка, поновіть сторінку і спробуйте ще раз.'
+    )
+);
